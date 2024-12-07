d6720b8a3ce9e7a776ca522fdb2d73a209d3571f
SheikhAbuzar245 <abuzarsarfraz963@gmail.com>
2024-12-07 23:23:55 +0500

    testing

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="container">
+        <div className="header">
+          <img src={Logo} alt="" />
+
+          <ul className="nav-links">
+            <li>Menu</li>
+            <li>Location</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+      </div>
+
+    </>
+  )
+}
+
+export default App
